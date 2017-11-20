0684524d9ab6a1d099f295c8f4ae4b23067520c5
daymickcorr <mickeycorriveau@hotmail.com>
2017-11-20 14:27:00 -0500

    depenedencies

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <title>Online Teaching</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="Dependencies/bootstrap-4.0.0/css/bootstrap.css">
+</head>
+<body>
+	<div>
+		<nav class="navbar navbar-inverse bg-inverse">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="#">
+						<img src="Dependencies/Images/OnlineTeaching_Logo.svg" alt="OnlineTeaching_Logo"/>
+						<span>Online Teaching</span>
+					</a>
+					
+				</div>
+			</div>
+		</nav>
+	</div>
+</body>
+</html>
